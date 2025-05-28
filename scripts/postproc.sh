a7a20bdafb59bc63ffc858e95df2b5608d66b6d7
Yibei Chen <yibeichan@gmail.com>
2025-05-28 17:56:31 -0400

    Update postproc.sh

--- scripts/postproc.sh
+++ scripts/postproc.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=postproc_friends
-#SBATCH --partition=normal
-#SBATCH --output=../logs/postproc_friends_%j.out
-#SBATCH --error=../logs/postproc_friends_%j.err
-#SBATCH --time=01:00:00
+#SBATCH --job-name=postproc_hcptrt
+#SBATCH --partition=mit_normal
+#SBATCH --output=../logs/postproc_hcptrt_%j.out
+#SBATCH --error=../logs/postproc_hcptrt_%j.err
+#SBATCH --time=00:30:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
 #SBATCH --array=0-5
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=yibei@mit.edu
 
-source $HOME/miniconda3/etc/profile.d/conda.sh
-
-# Activate your Conda environment
-conda activate neuro
+source ~/.bashrc  # or source /etc/profile.d/conda.sh
+# activate your env
 
 sub_ids=("sub-01" "sub-02" "sub-03" "sub-04" "sub-05" "sub-06")
 
@@ -22,4 +20,4 @@ TASK_ID=${sub_ids[$SLURM_ARRAY_TASK_ID]}
 
 echo "Processing: $TASK_ID"
 
-python postproc.py "${TASK_ID}" "friends"
+python 00_postproc.py "${TASK_ID}" "hcptrt"
